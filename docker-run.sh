f6533de62b8b1ef05150bdad9dd8c46655eccbcd
miyago9267 <miyagomizugen@gmail.com>
2024-10-07 12:53:45 +0800

    chore: make dockerfile

---
+++ docker-run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+user="cli"
+name="cli-bot"
+
+docker build \
+    $@ -t $user/$name:latest . || exit
+[ "$(docker ps | grep $name)" ] && docker kill $name
+[ "$(docker ps -a | grep $name)" ] && docker rm $name
+
+docker run \
+	-itd \
+	-u $(id -u):$(id -g) \
+	--name $name \
+    --network host \
+	$user/$name:latest
